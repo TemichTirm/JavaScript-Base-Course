3359893b29de834488b718067a75902745ed54a2
Temich <temicht@gmail.com>
2021-07-28 23:13:04 +0300

    Realize Homework №6.2

--- HomeWork№6/cart.js
+++ HomeWork№6/cart.js
@@ -11,14 +11,15 @@ function Product(id, productName, brand, price, quantity) {
 // Создание корзины и инициализация пяти продуктов
 let productList = {
     products: [
-        new Product(id = 1, productName = "Туфли", brand = "Carlo Pazolini", price = 100),
+        new Product(id = 1, productName = "Туфли мужские", brand = "Carlo Pazolini", price = 100),
         new Product(id = 2, productName = "Шляпа", brand = "Borsalino", price = 20),
-        new Product(id = 3, productName = "Пальто", brand = "Finn Flare", price = 300),
-        new Product(id = 4, productName = "Рубашка", brand = "Henderson", price = 80),
+        new Product(id = 3, productName = "Пальто женское", brand = "Finn Flare", price = 300),
+        new Product(id = 4, productName = "Сорочка", brand = "Henderson", price = 80),
         new Product(id = 5, productName = "Футболка", brand = "Lacoste", price = 40),
         new Product(id = 6, productName = "Галстук", brand = "Henderson", price = 15),
         new Product(id = 7, productName = "Брюки", brand = "O'stin", price = 180),
         new Product(id = 8, productName = "Платье", brand = "D&G", price = 250),
+        new Product(id = 9, productName = "Сумочка", brand = "GUCCI", price = 500),
     ],
 
     getProductById: function(id) {
@@ -191,4 +192,54 @@ function deleteFromCartButtonClick (event) {
         cart.products.splice(position-1, 1);
     }
     createCartTable();
+};
+
+function onCliclImage(event) {
+    let path = event.target.src;
+    const galery = document.getElementsByClassName("galleryContent")[0];
+    let str = `<img id="image" class="mainImage" src="${path}">`
+    galery.innerHTML = str;
+};
+
+window.addEventListener("load", init);
+
+function setImagesClickHandler() {
+    let thumbnails = document.getElementsByClassName("thumbnails")
+    for (let thubnail of thumbnails) {
+        thubnail.addEventListener("click", onCliclImage);
+    };
+};
+
+function init() {
+    createProductTable();
+    const rows = document.getElementById("list-of-products").childNodes;
+    for (let row of rows) {
+        let cells = row.childNodes;
+        for (let i = 0; i < cells.length-1; i++) {
+            cells[i].addEventListener("mouseover", onFocusRowHandler);
+        };
+    };   
+};
+
+function onFocusRowHandler(event) {    
+    let thumbnailsWindow = document.getElementsByClassName("thumbnailsWindow")[0];
+    let galleryContent = document.getElementsByClassName("galleryContent")[0];
+    thumbnailsWindow.innerHTML = "";
+    let id = +event.target.parentNode.childNodes[0].innerText;
+    for (let i = 1; i <= 5; i++) {
+        let path = `images/galery/${id}/${id}_${i}.jpg`;
+        if ((id === 2 || id === 6) && i > 3) {
+            break;
+        }
+        let element = document.createElement("img");
+        if (i === 1) {
+            let str = `<img id="image" class="mainImage" src="${path}">`;
+            galleryContent.innerHTML = str;
+        }
+        element.id = `image-thumbnail-${i}`;
+        element.className = "thumbnails";
+        element.src = `${path}`;
+        thumbnailsWindow.appendChild(element);
+    };
+    setImagesClickHandler();
 };
